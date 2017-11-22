d226787fc7039b06b73daf12064a472fbb9090e5
seshassai33 <seshasai33@gmail.com>
2017-11-21 19:25:35 -0600

    WordCount commit

---
+++ WordCount/src/WordCount.java
@@ -0,0 +1,8 @@
+
+public class WordCount {
+	public static void main(String[] args) {
+		String s1=new String("Hello! How  are you?");
+		String[] words=s1.split(" ");
+		System.out.println(words.length);
+	}
+}
