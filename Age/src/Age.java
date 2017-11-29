b569800648afef1ad92e276b6b5d3c6f5128e285
seshassai33 <seshasai33@gmail.com>
2017-11-28 19:31:53 -0600

    updated

--- Age/src/Age.java
+++ Age/src/Age.java
@@ -7,30 +7,30 @@ public class Age {
 
 	public static void main(String[] args) throws ParseException{
 		Calendar todayCalendar=Calendar.getInstance();
-		SimpleDateFormat df=new SimpleDateFormat("dd/MM/yyyy");
 		Calendar dobCalendar=Calendar.getInstance();
+		SimpleDateFormat df=new SimpleDateFormat("dd/MM/yyyy");
 		Date dob=df.parse("04/05/1995");
 		dobCalendar.setTime(dob);
 		int years=todayCalendar.get(Calendar.YEAR)-dobCalendar.get(Calendar.YEAR);
+		int months=todayCalendar.get(Calendar.MONTH)-dobCalendar.get(Calendar.MONTH);
 		if(todayCalendar.get(Calendar.DAY_OF_YEAR)>dobCalendar.get(Calendar.DAY_OF_YEAR)) {
-			System.out.println(years+"Years");
+			System.out.println(years);
 		}
 		else if(todayCalendar.get(Calendar.DAY_OF_YEAR)<dobCalendar.get(Calendar.DAY_OF_YEAR)) {
-			System.out.println(--years+"Years");
+			System.out.println(years=years-1);
 		}
 		else {
 			System.out.println("Its your birthday");
 		}
 		if(todayCalendar.get(Calendar.MONTH)>dobCalendar.get(Calendar.MONTH)) {
-			int months=todayCalendar.get(Calendar.MONTH)-dobCalendar.get(Calendar.MONTH);
-			System.out.println(months+"Months");
+			
+			System.out.println(months);
 		}
 		else if(todayCalendar.get(Calendar.MONTH)<dobCalendar.get(Calendar.MONTH)) {
-			int months=todayCalendar.get(Calendar.MONTH)-dobCalendar.get(Calendar.MONTH);
-			System.out.println(12+months+"months");
+			System.out.println(12+months);
 		}
 		else {
-			System.out.println("0 months");
+			System.out.println("Birthday month");
 		}
 	}
 }
